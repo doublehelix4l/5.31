3c178e48090398b70112d88fb5e1ca9158c4db81
doublehelix4l <jakercrockett@gmail.com>
2023-06-01 08:33:23 -0500

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
+import './index.css'
 
+let puppyPass = 0
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const puppyPass = puppies.find((pup)=> pup.id === featPupId)
+
+  function handleClick(obj) {
+   //setFeatPupId(obj)
+    //console.log("puppy id: ", setFeatPupId(obj.id))
+  }
 
   return (
-    <>
-      <div>
-       
-      </div>
-   
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+  <div>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+  </div>
+)}
+    </div>
+  );
 }
 
 export default App
